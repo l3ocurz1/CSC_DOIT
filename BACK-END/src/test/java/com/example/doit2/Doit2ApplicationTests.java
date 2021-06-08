b998e71dd02bf145a4ea9fdd1085b9324bf62965
emanuel <emanuel.fermani@studenti.unicam.it>
2021-06-08 16:35:28 +0200

    Test seconda iterazione

---
+++ BACK-END/src/test/java/com/example/doit2/Doit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Doit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
